4f4a0c3a8772e52d1efb250e6b2d8ece2d281a42
a-jadczak <arkadiusz.jadczak7@gmail.com>
2026-09-10 12:32:43 +0200

    feat: add badge

---
+++ src/components/ui/Badge/Badge.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Badge.module.scss';
+
+const Badge = ({ title }: { title: string }) => {
+  return <div className={styles.badge}>{title}</div>;
+};
+
+export default Badge;
