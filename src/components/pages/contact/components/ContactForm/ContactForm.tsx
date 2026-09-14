6c9265ed11d9900f1fdf6dbd5e63a484347b19b3
a-jadczak <arkadiusz.jadczak7@gmail.com>
2026-09-14 09:29:39 +0200

    feat: add contact form heading elements and decoration

--- src/components/pages/contact/components/ContactForm/ContactForm.tsx
+++ src/components/pages/contact/components/ContactForm/ContactForm.tsx
@@ -11,42 +11,52 @@ const ContactForm = () => {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleFormSubmit}>
-      <label className={styles.label} htmlFor="name">
-        <span className={styles.labelContent}>name: </span>
-        <input className={styles.input} type="text" placeholder="Your name" name="name" />
-      </label>
-      <label className={styles.label} htmlFor="email">
-        <span className={styles.labelContent}>email: </span>
-        <input className={styles.input} type="text" placeholder="you@example.com" name="email" />
-      </label>
-      <label className={styles.label} htmlFor="subject">
-        <span className={styles.labelContent}>subject: </span>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="What's this about?"
-          name="subject"
+    <div className={`clipped-corners ${styles.container}`} aria-label="Contact form">
+      <div className={styles.containerTitle} aria-hidden={true}>
+        <span>contact@portfolio:~</span>
+        <div className={styles.circleContainer} aria-hidden={true}>
+          <div className={styles.circle}></div>
+          <div className={styles.circle}></div>
+          <div className={styles.circle}></div>
+        </div>
+      </div>
+      <form onSubmit={handleFormSubmit}>
+        <label className={styles.label} htmlFor="name">
+          <span className={styles.labelContent}>name: </span>
+          <input className={styles.input} type="text" placeholder="Your name" name="name" />
+        </label>
+        <label className={styles.label} htmlFor="email">
+          <span className={styles.labelContent}>email: </span>
+          <input className={styles.input} type="text" placeholder="you@example.com" name="email" />
+        </label>
+        <label className={styles.label} htmlFor="subject">
+          <span className={styles.labelContent}>subject: </span>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="What's this about?"
+            name="subject"
+          />
+        </label>
+        <label className={styles.label} htmlFor="message">
+          <span className={styles.labelContent}>message: </span>
+          <textarea
+            className={`clipped-corners ${styles.messageContainer}`}
+            rows={5}
+            name="message"
+            placeholder="Tell me about your project..."
+          ></textarea>
+        </label>
+        <Button
+          as="button"
+          text="Send message"
+          variant="primary"
+          Icon={Send}
+          type="submit"
+          style={{ width: '100%' }}
         />
-      </label>
-      <label className={styles.label} htmlFor="message">
-        <span className={styles.labelContent}>message: </span>
-        <textarea
-          className={`clipped-corners ${styles.messageContainer}`}
-          rows={5}
-          name="message"
-          placeholder="Tell me about your project..."
-        ></textarea>
-      </label>
-      <Button
-        as="button"
-        text="Send message"
-        variant="primary"
-        Icon={Send}
-        type="submit"
-        style={{ width: '100%' }}
-      />
-    </form>
+      </form>
+    </div>
   );
 };
 
